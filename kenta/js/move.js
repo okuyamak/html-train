ae06db873c3d266309edd82cb607e82f01f925dd
okuyamak <ko215ok@gmail.com>
2019-12-14 11:41:29 +0100

    added kenta directory

--- kenta/js/move.js
+++ kenta/js/move.js
@@ -48,18 +48,17 @@ function updateName() {
 /* interactive skill bar */
 /**********************************/
 var i = 0;
-function move(bar) {
+function move(skillID) {
   if (i == 0) {
     i = 1;
-    var elem = document.getElementsByClassName(bar);
+    var elem = document.ElementById(skillID);
     var width = 1;
     var id = setInterval(frame, 10);
     function frame() {
       if (width >= 100) {
         clearInterval(id);
         i = 0;
-      } 
-			else {
+      } else {
         width++;
         elem.style.width = width + "%";
       }
@@ -67,7 +66,9 @@ function move(bar) {
   }
 }
 
-move("skills");
+move("#jqBar, #jqBar2");
+
+
 
 
 /**********************************/
